001e205b7e7c2a2e8241fed252e46fdb375da952
Amanda Costa <amanda.costa@liferay.com>
2022-04-07 16:48:13 -0300

    test file (ukkonen only)

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ path_files = './data/test_archive/'
 # padrões definidos com base aleatória entre os textos testados
 patts = {
     #"dna": "ACACGTCGGTAACCAGTGCACAGGGAATCTGCTTCACAGCCGCCCCTCCCCTGCAGCAGGGAGGAGGTGTCACCAAAGCAGGGCCTGTCGGAGTCTCTGG",
-    #"english": "To Americans it is incredible that the class war should develop to such a pitch. But I have personall",
+    "english": "To Americans it is incredible that the class war should develop to such a pitch. But I have personall",
     "pitches": "0H?JAH?0G>7HG7F2HFA2E<5FE5D0FD0?3CDHC3O/JCOJ/A/JCJAMJAMJ8KH7HJ5K7JMG87HK<GA>?CH>JDAJ?CHJHAKCHJMGDKCH",
     #"proteins": "QDCLCQMLDLVKPRTEGKITLQDLKRCKLANVFFDTFFNIEKYLDHEQKEQISLLRDGDSGGPELSDWEKYAAEEYDILVAEETAGEPWEDGFEAELSPV",
     "sources": "hile (xl_priv->xl_rx_ring[xl_priv->rx_ring_tail].framestatus & (RXUPDCOMPLETE | RXUPDFULL) ) { /* De",
@@ -14,8 +14,10 @@ patts = {
 
 # Testes para casamento exato de padrões 
 # Variando o tamanho do prefixo do padrão de 1 até 100, testa todos os algoritmos do projeto e o grep
+
+alg = "Ukkonen"
 for file in patts.keys():
-    for alg in ["AhoCorasick", "BruteForce", "KMP", "Ukkonen", "WuManber", "grep"]:
+    #for alg in ["AhoCorasick", "BruteForce", "KMP", "Ukkonen", "WuManber", "grep"]:
         print(f"-------------This is {alg} with {file} file-------------")
         for end in range(1,101):
             print("-------------")
@@ -30,14 +32,14 @@ for file in patts.keys():
 # Testes para casamento aproximado de padrões
 # Utiliza padrão de erro variante entre 0 e m-1 (m sendo tamanho do padrão), no nosso caso, padrão fixo de tamanho 16
 for file in patts.keys():
-    for alg in ["WuManber", "Ukkonen", "agrep"]:
-        print(f"-------------This is {alg} with {file} file-------------")
-        for r in range(0, 16):
-            print("-------------")
-            print(f"{alg} - {file} - edit: {str(r)}")
-            print(r, len(patts[file][0:16]))
-            if (alg != "agrep"):
-                os.system(f"time ./bin/pmt --count --edit {r} --algorithm {alg} \"{patts[file][0:16]}\" {path_files + file}")
-            else:
-                os.system(f"time agrep -c {str(r)} \"{patts[file][0:16]}\" {path_files + file}")
-        print(f"-------------END of {alg} with {file} file-------------")
+    #for alg in ["WuManber", "Ukkonen", "agrep"]:
+    print(f"-------------This is {alg} with {file} file - Aproximated Matching -------------")
+    for r in range(0, 16):
+        print("-------------")
+        print(f"{alg} - {file} - edit: {str(r)}")
+        print(r, len(patts[file][0:16]))
+        if (alg != "agrep"):
+            os.system(f"time ./bin/pmt --count --edit {r} --algorithm {alg} \"{patts[file][0:16]}\" {path_files + file}")
+        else:
+            os.system(f"time agrep -c {str(r)} \"{patts[file][0:16]}\" {path_files + file}")
+    print(f"-------------END of {alg} with {file} file (AP) -------------")
